48d169fc122b824e4aa50277618233e289e5c3fb
akshaypatil1 <php.akshay@gmail.com>
2014-08-19 17:26:10 +0530

    update

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.get( '/api/db/list/id/:id', function ( objRequest, objResponse ){
 /**
  * All active list with ID
  */
-app.post( '/api/db/list', function ( objRequest, objResponse ){
+app.post( '/api/db/list/oneRecord', function ( objRequest, objResponse ){
     // Set response type from text/html to application/json
     //var id = objRequest.params.id;
 	var id = objRequest.body.id;
@@ -161,7 +161,138 @@ app.post( '/api/db/list', function ( objRequest, objResponse ){
     });
 });
 
+/*
+*	Inserting data in MySQL DB
+*/
 
+app.post( '/api/db/list/insertRecord', function ( objRequest, objResponse ){
+    // Set response type from text/html to application/json
+    //var id = objRequest.params.id;
+	var name = objRequest.body.name;
+	objResponse.setHeader( 'content-type', 'application/json' );
+
+    // Get a connection to the database
+    db_pool.getConnection( function ( objError, objConnection ){
+
+        // check for any connection errors
+        if( objError ){
+
+            // There was an error, so send JSON with an error message and an HTTP status of 503 (Service Unavailable)
+            sendError( objResponse, 503, 'error', 'connection', objError );
+
+        }else{
+			//console.log('INSERT INTO list ("name") VALUES ("'+name+'");');
+            objConnection.query(
+                'INSERT INTO list (id, name) VALUES (NULL,"'+name+'") ',
+                function ( objError, objRows, objFields ){
+                    if( objError ){
+                        
+                        // Couldn't get the query to run, so send JSON with an error message and an HTTP status of 500 (Internal Server Error)
+                        sendError( objResponse, 500, 'error', 'query', objError );
+                    
+                    }else{
+                    
+                        // We have query results back, so lets put the results in JSON and return them
+                        objResponse.send({
+                            result      : 'success',
+                            //err         : '',
+                            //err_type    : '',
+                            //fields      : objFields,
+                            rows        : objRows,
+                            length      : objRows.length
+                        });
+
+                    }
+                }
+            );
+            objConnection.release();
+        }
+    });
+});
+
+/**
+ * You can fire two queries as shown below.
+ *	here i'm taking no_view as per 'id' from DB then updating that in DB
+ */
+app.post( '/api/db/list/views', function ( objRequest, objResponse ){
+    // Set response type from text/html to application/json
+    //var id = objRequest.params.id;
+	var id = objRequest.body.id;
+	objResponse.setHeader( 'content-type', 'application/json' );
+
+    // Get a connection to the database
+    db_pool.getConnection( function ( objError, objConnection ){
+
+        // check for any connection errors
+        if( objError ){
+
+            // There was an error, so send JSON with an error message and an HTTP status of 503 (Service Unavailable)
+            sendError( objResponse, 503, 'error', 'connection', objError );
+
+        }else{
+
+            objConnection.query(
+                'SELECT * FROM list where id = '+id+'',
+                function ( objError, objRows, objFields ){
+                    if( objError ){
+                        
+                        // Couldn't get the query to run, so send JSON with an error message and an HTTP status of 500 (Internal Server Error)
+                        sendError( objResponse, 500, 'error', 'query', objError );
+                    
+                    }
+					else
+					{
+						//console.log(objRows);
+						
+						for (var i in objRows) {
+							//console.log('Views : ', objRows[i].no_views);
+							views = objRows[i].no_views;
+						}
+						views = views+1;
+						//---------------------------------------------------------------------
+						objConnection.query(
+								"UPDATE `list` SET `no_views` = '"+views+"' WHERE `list`.`id` = "+id+";",
+								function ( objError, objRows, objFields ){
+									if( objError ){
+										
+										// Couldn't get the query to run, so send JSON with an error message and an HTTP status of 500 (Internal Server Error)
+										sendError( objResponse, 500, 'error', 'query', objError );
+									
+									}
+									else
+									{
+														
+											// We have query results back, so lets put the results in JSON and return them
+											objResponse.send({
+												result      : 'success: update views ',
+												views         : views,
+												//err_type    : '',
+												//fields      : objFields,
+												//rows        : objRows,
+												//length      : objRows.length
+											});
+
+									}
+								}
+							);
+						//------------------------------------------------------------------------
+							// We have query results back, so lets put the results in JSON and return them
+						/*	objResponse.send({
+								result      : 'success',
+								//views         : views,
+								//err_type    : '',
+								//fields      : objFields,
+								//rows        : objRows,
+								//length      : objRows.length
+							});*/
+
+                    }
+                }
+            );
+            objConnection.release();
+        }
+    });
+});
 
 
 /**
